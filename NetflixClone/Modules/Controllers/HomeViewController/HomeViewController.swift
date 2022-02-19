b5973e1434bf52bd0e1cf608f1ca7739c520aa7a
Noureddine Legragui <Nour-Eddine.Legragui@ibm.com>
2022-02-19 17:55:49 +0100

    #6 - Home View - create tableview

--- NetflixClone/Modules/Controllers/HomeViewController/HomeViewController.swift
+++ NetflixClone/Modules/Controllers/HomeViewController/HomeViewController.swift
@@ -8,11 +8,53 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let homeTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self , forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
 
+    // MARK: - Overrides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBlue
+        view.addSubview(homeTableView)
+        
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+    
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeTableView.frame = view.bounds
     }
 
 }
+
+// MARK: - UITableViewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World!"
+        return cell
+    }
+
+}
+
