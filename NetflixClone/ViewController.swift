db4e99bd17ddaff827b6bcbb919218fa638bc278
Noureddine Legragui <Nour-Eddine.Legragui@ibm.com>
2022-02-19 16:02:38 +0100

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Noureddine Legragui on 19/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
